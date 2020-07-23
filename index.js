4917f77a0af0afc51d51c343acc2578c44c760a3
KozaX <55134615+KozaX@users.noreply.github.com>
2020-07-23 19:13:54 +0200

    done

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,6 +22,51 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'carne de porco a alentejana',
+      level: 'Amateur Chef',
+      ingredients: [
+        'Carne de porco',
+        'batata',
+        'ameijoas',
+        'coentros',
+        'azeitona'
+      ],
+      cuisine: 'Portuguese',
+      dishType: 'main_course',
+      image:
+        'https://lh3.googleusercontent.com/proxy/t6dhqa8_qLLle1xPt4flCCd_bXM8jq69P0KPogu-XI1qQECbvKUJ4ecij_13fSKqfERyyao9emZXLBBvukn9RJycIbk0g0DX6rE9XkOQs1smQWDOunhYiohODREiU4255LMnLhKazNsAwluB4oqq-_4',
+      duration: 55,
+      creator: 'Mrs. Alentejana',
+      created: new Date(1974, 3, 25) // month 0 indexed
+    });
+  })
+  .then(recipe => {
+    console.log('This is what was returned', recipe);
+
+    return Recipe.insertMany(data);
+  })
+  .then(data => {
+    data.map(recipe => {
+      console.log(recipe.title);
+    });
+
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 }
+    );
+  })
+  .then(() => {
+    console.log('Rigatoni alla Genovese: duration successfully updated');
+
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Successfully deleted the carrot cake from the DB');
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('disconnected from the database');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
